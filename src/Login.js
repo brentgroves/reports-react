e30cafcae5be2be0cb4dde94c7b3718abdbf6ffb
brentgroves <brent.groves@gmail.com>
2022-12-15 20:27:45 -0500

    updated source code

--- src/Login.js
+++ src/Login.js
@@ -84,8 +84,11 @@ const Login = () => {
             {/* <a class="button button-primary block" href="http://localhost:3030/oauth/microsoft">
               Login with OAuth
             </a>            https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=12345 */}
-            <a class="button button-primary block" href="https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Foauth%2Fmicrosoft&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=12345">
+            {/* <a class="button button-primary block" href="https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Foauth%2Fmicrosoft&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=12345">
               Login with OAuth
+            </a>             */}
+            <a class="button button-primary block" href="http://localhost:3030/auth">
+              Login to Mobex
             </a>            
 
           </form>
